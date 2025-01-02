ccfa49c7709e494a73b8740e44e090311385e671
shj <josh2kv@gmail.com>
2025-01-02 16:26:10 +0900

    [hw9] Style global-header

--- hw/hw9-book-search-app/src/app/app-routing.module.ts
+++ hw/hw9-book-search-app/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { WishlistComponent } from './pages/wishlist/wishlist.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'wishlist', component: WishlistComponent },
 ];
 
