02bb6de68cc084db2fa3d91bbd80ae47d9d3542c
shj <josh2kv@gmail.com>
2025-01-05 18:03:13 +0900

    [hw10] Fix document is `undefined` error

--- hw/hw10-notflex-frontend/src/app/core/services/theme/theme.service.ts
+++ hw/hw10-notflex-frontend/src/app/core/services/theme/theme.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { LIGHT_MODE_ROUTES } from '@core/config/routes';
 import { ThemeMode } from '@shared/interfaces/common';
@@ -8,15 +9,19 @@ import { BehaviorSubject, filter } from 'rxjs';
   providedIn: 'root',
 })
 export class ThemeService {
+  private platformId = inject(PLATFORM_ID);
   private lightModeRoutes: string[] = LIGHT_MODE_ROUTES;
   private themeSubject = new BehaviorSubject<ThemeMode>('dark-mode');
   themeMode$ = this.themeSubject.asObservable();
 
   constructor(private router: Router) {
-    console.log(this.router.url);
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((event) => this.onRouteChange(event.url));
+    if (isPlatformBrowser(this.platformId)) {
+      this.onRouteChange(this.router.url);
+
+      this.router.events
+        .pipe(filter((event) => event instanceof NavigationEnd))
+        .subscribe((event) => this.onRouteChange(event.url));
+    }
   }
 
   private onRouteChange(route: string): void {
@@ -30,9 +35,12 @@ export class ThemeService {
   }
 
   setThemeMode(mode: ThemeMode) {
-    const html = document.documentElement;
-    html.classList.remove('light-mode', 'dark-mode');
-    html.classList.add(mode);
     this.themeSubject.next(mode);
+
+    if (isPlatformBrowser(this.platformId)) {
+      const html = document.documentElement;
+      html.classList.remove('light-mode', 'dark-mode');
+      html.classList.add(mode);
+    }
   }
 }
