a338b09dd7d196d373b2fe2e4df88f448056bedf
shj <josh2kv@gmail.com>
2025-01-16 18:15:37 +0900

    [hw12] Implement contact-us page

--- hw/hw12-interview-form-router/src/app/pages/contact-us/contact-us.component.ts
+++ hw/hw12-interview-form-router/src/app/pages/contact-us/contact-us.component.ts
@@ -1,12 +1,40 @@
+import { ContactFormControls } from '@/shared/interfaces/contact-us';
 import { Component } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   standalone: false,
-  
+
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.scss'
+  styleUrl: './contact-us.component.scss',
 })
 export class ContactUsComponent {
+  contactForm: FormGroup<ContactFormControls>;
+  submitted = false;
 
+  constructor(private fb: NonNullableFormBuilder) {
+    this.contactForm = this.fb.group<ContactFormControls>({
+      name: this.fb.control('', [Validators.required]),
+      message: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.errors);
+    console.log(this.contactForm.get('name')?.hasError('required'));
+    console.log(this.contactForm.value);
+    this.submitted = true;
+  }
 }
--- hw/hw12-interview-form-router/src/app/pages/contact-us/contact-us.module.ts
+++ hw/hw12-interview-form-router/src/app/pages/contact-us/contact-us.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ContactUsRoutingModule } from './contact-us-routing.module';
 import { ContactUsComponent } from './contact-us.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    ContactUsComponent
-  ],
-  imports: [
-    CommonModule,
-    ContactUsRoutingModule
-  ]
+  declarations: [ContactUsComponent],
+  imports: [CommonModule, ContactUsRoutingModule, ReactiveFormsModule],
 })
-export class ContactUsModule { }
+export class ContactUsModule {}
