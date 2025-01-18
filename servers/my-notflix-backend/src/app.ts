2f733f8f230c3acd046c78e752330e42a3fe585f
shj <josh2kv@gmail.com>
2025-01-18 11:00:14 +0900

    [hw13-b] Environment Setup and Basic Server

--- servers/my-notflix-backend/src/app.ts
+++ servers/my-notflix-backend/src/app.ts
@@ -1,9 +1,19 @@
-import express from "express";
+import express, { Express } from "express";
+import "./core/env.config";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+(() => {
+  const app: Express = express();
+  const port = process.env.PORT || 4231;
 
-export default app;
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Hello nodejs microsddervices!",
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port: http://localhost:${port}`);
+  });
+})();
