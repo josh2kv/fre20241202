20f25883c92643d8ccd1d68917ef2ae8a586efec
shj <josh2kv@gmail.com>
2025-01-26 17:53:19 +0900

    [hw14.19] refactor movie-list with infinite scrolling

--- hw/hw10-notflix-frontend/src/app/features/movie-list/components/movie-grid/movie-grid.component.ts
+++ hw/hw10-notflix-frontend/src/app/features/movie-list/components/movie-grid/movie-grid.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '@core/services/movie/movie.service';
-import { DataWithPagination } from '@shared/interfaces/common';
+import { DataWithPagination, PaginationMeta } from '@shared/interfaces/common';
 import { Movie, MoviesWithPagination } from '@shared/interfaces/movie';
 
 @Component({
@@ -11,24 +11,31 @@ import { Movie, MoviesWithPagination } from '@shared/interfaces/movie';
   styleUrl: './movie-grid.component.scss',
 })
 export class MovieGridComponent implements OnInit {
-  movies: DataWithPagination<Movie[]> | null = null;
-  loading = false;
+  page = 0;
+  movies: Movie[] = [];
+  meta: PaginationMeta | null = null;
+  isLoading = false;
   error = '';
 
   constructor(private moviesApiService: MovieService) {}
 
   ngOnInit(): void {
-    this.moviesApiService.getMovies().subscribe({
-      next: (moviesWithPagination) => {
-        this.movies = moviesWithPagination;
-      },
-      error: (error) => {
-        console.error('Error fetching movies:', error);
-        this.error = 'Failed to fetch movies. Please try again.';
-      },
-      complete: () => {
-        this.loading = false;
-      },
+    this.loadNextMovies();
+  }
+
+  loadNextMovies(): void {
+    this.isLoading = true;
+    const page = (this.meta?.page ?? 1) + 1;
+    this.moviesApiService.getMovies(page).subscribe((newMovies) => {
+      this.movies = [...this.movies, ...newMovies.data];
+      this.meta = newMovies.meta;
+      this.isLoading = false;
     });
+  }
+
+  onScroll(): void {
+    if (this.meta?.hasNextPage) {
+      this.loadNextMovies();
+    }
   }
 }
