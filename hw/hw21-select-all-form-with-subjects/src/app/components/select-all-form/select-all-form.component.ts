af6ccfb6264ce789f0d9c379e13b4749ed56c28f
shj <josh2kv@gmail.com>
2025-04-03 18:24:51 +0900

    [hw21] Init app

---
+++ hw/hw21-select-all-form-with-subjects/src/app/components/select-all-form/select-all-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-all-form',
+  standalone: false,
+  templateUrl: './select-all-form.component.html',
+  styleUrl: './select-all-form.component.scss'
+})
+export class SelectAllFormComponent {
+
+}
