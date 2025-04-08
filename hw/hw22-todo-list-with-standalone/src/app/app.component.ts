1e69f978fc1d3cc9bf27bf0a62c239144fcd2f9e
shj <josh2kv@gmail.com>
2025-04-08 18:42:10 +0900

    [hw22] Implement todo-list

--- hw/hw22-todo-list-with-standalone/src/app/app.component.ts
+++ hw/hw22-todo-list-with-standalone/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'hw22-todo-list-with-standalone';
