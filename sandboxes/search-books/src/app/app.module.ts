9e128c9b99e23eddb6aa4b71e2dabda3ceb1f52a
shj <josh2kv@gmail.com>
2025-04-14 14:18:18 +0900

    [search-books] Implement search feature

--- sandboxes/search-books/src/app/app.module.ts
+++ sandboxes/search-books/src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { WishlistPageComponent } from './pages/wishlist-page/wishlist-page.component';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent,
-    WishlistPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomePageComponent, WishlistPageComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- sandboxes/search-books/src/app/pages/home-page/home-page.component.ts
+++ sandboxes/search-books/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,88 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map, tap } from 'rxjs/operators';
+
+interface ResBooks {
+  kind: string;
+  totalItems: number;
+  items: ResBookItem[];
+}
+
+interface ResBookItem {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: {
+    title: string;
+    publisher: string;
+    publishedDate: string;
+    description: string;
+    imageLinks: {
+      thumbnail: string;
+    };
+  };
+}
+
+interface BookItem {
+  thumbnail: string;
+  name: string;
+  publisher: string;
+  publishedDate: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-home-page',
   standalone: false,
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  API_URL = 'https://www.googleapis.com/books/v1/volumes';
+  result: BookItem[] = [];
+  searchControl: FormControl<string>;
 
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.searchControl = this.fb.control('', { nonNullable: true });
+  }
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(distinctUntilChanged(), debounceTime(400))
+      .subscribe((value) => {
+        this.onSearch(value);
+      });
+  }
+
+  onSearch(q: string | null) {
+    if (!q) {
+      this.result = [];
+      return;
+    }
+
+    const params = new HttpParams({
+      fromObject: {
+        q,
+      },
+    });
+
+    this.http
+      .get<ResBooks>(this.API_URL, { params })
+      .pipe(
+        map((res) =>
+          res.items.map((item) => ({
+            thumbnail: item.volumeInfo.imageLinks.thumbnail,
+            name: item.volumeInfo.title,
+            publisher: item.volumeInfo.publisher,
+            publishedDate: item.volumeInfo.publishedDate,
+            description: item.volumeInfo.description,
+          }))
+        )
+      )
+      .subscribe((books) => {
+        this.result = books;
+      });
+  }
 }
