bb744400cdd66113f85b9fef7e003a6d5b16098e
shj <josh2kv@gmail.com>
2025-04-14 12:58:22 +0900

    [hw25] elegant answer

--- hw/hw25-traffic-light/src/main.ts
+++ hw/hw25-traffic-light/src/main.ts
@@ -4,41 +4,69 @@ const redLight$ = document.getElementById("red-light");
 const yellowLight$ = document.getElementById("yellow-light");
 const greenLight$ = document.getElementById("green-light");
 
-redLight$?.classList.toggle("on");
-setTimeout(() => {
-  redLight$?.classList.toggle("on");
-  yellowLight$?.classList.remove("on");
-  greenLight$?.classList.toggle("on");
-}, 4000);
+const DURATION_RED = 4000;
+const DURATION_YELLOW = 2000;
+const DURATION_GREEN = 4000;
 
-setTimeout(() => {
-  redLight$?.classList.remove("on");
-  yellowLight$?.classList.toggle("on");
-  greenLight$?.classList.toggle("on");
-}, 8000);
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-setTimeout(() => {
-  redLight$?.classList.toggle("on");
-  yellowLight$?.classList.toggle("on");
-  greenLight$?.classList.remove("on");
-}, 10000);
+const changeLights = (red: boolean, yellow: boolean, green: boolean) => {
+  redLight$?.classList.toggle("on", red);
+  yellowLight$?.classList.toggle("on", yellow);
+  greenLight$?.classList.toggle("on", green);
+};
 
-setInterval(() => {
-  setTimeout(() => {
-    redLight$?.classList.toggle("on");
-    yellowLight$?.classList.remove("on");
-    greenLight$?.classList.toggle("on");
-  }, 4000);
+const initialize = async () => {
+  while (true) {
+    changeLights(true, false, false);
+    await delay(DURATION_RED);
 
-  setTimeout(() => {
-    redLight$?.classList.remove("on");
-    yellowLight$?.classList.toggle("on");
-    greenLight$?.classList.toggle("on");
-  }, 8000);
+    changeLights(false, false, true);
+    await delay(DURATION_GREEN);
 
-  setTimeout(() => {
-    redLight$?.classList.toggle("on");
-    yellowLight$?.classList.toggle("on");
-    greenLight$?.classList.remove("on");
-  }, 10000);
-}, 10000);
+    changeLights(false, true, false);
+    await delay(DURATION_YELLOW);
+  }
+};
+
+initialize();
+
+/*  My Awful Answer*/
+// redLight$?.classList.toggle("on");
+// setTimeout(() => {
+//   redLight$?.classList.toggle("on");
+//   yellowLight$?.classList.remove("on");
+//   greenLight$?.classList.toggle("on");
+// }, 4000);
+
+// setTimeout(() => {
+//   redLight$?.classList.remove("on");
+//   yellowLight$?.classList.toggle("on");
+//   greenLight$?.classList.toggle("on");
+// }, 8000);
+
+// setTimeout(() => {
+//   redLight$?.classList.toggle("on");
+//   yellowLight$?.classList.toggle("on");
+//   greenLight$?.classList.remove("on");
+// }, 10000);
+
+// setInterval(() => {
+//   setTimeout(() => {
+//     redLight$?.classList.toggle("on");
+//     yellowLight$?.classList.remove("on");
+//     greenLight$?.classList.toggle("on");
+//   }, 4000);
+
+//   setTimeout(() => {
+//     redLight$?.classList.remove("on");
+//     yellowLight$?.classList.toggle("on");
+//     greenLight$?.classList.toggle("on");
+//   }, 8000);
+
+//   setTimeout(() => {
+//     redLight$?.classList.toggle("on");
+//     yellowLight$?.classList.toggle("on");
+//     greenLight$?.classList.remove("on");
+//   }, 10000);
+// }, 10000);
