91e034bb8c7b33654cfc274b6183eabdbd3489c9
shj <josh2kv@gmail.com>
2025-05-10 19:45:15 +0900

    [hw29] Add pagination

--- hw/hw29-data-table-standalone/src/app/components/users-table/users-table.component.ts
+++ hw/hw29-data-table-standalone/src/app/components/users-table/users-table.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '@/app/services/user.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users-table',
@@ -11,15 +12,45 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './users-table.component.css',
 })
 export class UsersTableComponent implements OnInit {
-  private readonly route = inject(ActivatedRoute);
-  private readonly userService = inject(UserService);
+  readonly router = inject(Router);
+  readonly route = inject(ActivatedRoute);
+  readonly userService = inject(UserService);
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      this.userService.getUsers({
-        page: params['page'] || 1,
-        pageSize: params['pageSize'] || 10,
-      });
+    this.route.queryParams
+      .pipe(
+        tap((params) => {
+          const page = params['page'] ? +params['page'] : 1;
+          const pageSize = params['pageSize'] ? +params['pageSize'] : 10;
+
+          this.userService.getUsers({ page, pageSize }).subscribe();
+        })
+      )
+      .subscribe();
+  }
+
+  onPageSizeChange(pageSize: number | null) {
+    this.router.navigate([], {
+      queryParams: {
+        pageSize: pageSize || 10,
+        page: 1,
+      },
+    });
+  }
+
+  onNextPage() {
+    this.router.navigate([], {
+      queryParams: {
+        page: this.userService.meta().page + 1,
+      },
+    });
+  }
+
+  onPreviousPage() {
+    this.router.navigate([], {
+      queryParams: {
+        page: this.userService.meta().page - 1,
+      },
     });
   }
 }
--- hw/hw29-data-table-standalone/src/app/services/user.service.ts
+++ hw/hw29-data-table-standalone/src/app/services/user.service.ts
@@ -1,13 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { computed, inject, Injectable, signal } from '@angular/core';
-import { tap, finalize, catchError, map } from 'rxjs/operators';
+import { tap, finalize, catchError, map, delay } from 'rxjs/operators';
 
 export interface User {
   id: number;
   name: string;
   age: number;
   occupation: string;
+}
+
+export interface PaginationMeta {
+  total: number;
+  page: number;
+  pageSize: number;
+  totalPages: number;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
 }
 
 @Injectable({
@@ -17,10 +26,19 @@ export class UserService {
   private readonly baseUrl = 'data/users.json';
   private readonly http = inject(HttpClient);
   private readonly _users = signal<User[]>([]);
+  private readonly _meta = signal<PaginationMeta>({
+    total: 0,
+    page: 1,
+    pageSize: 10,
+    totalPages: 0,
+    hasNextPage: false,
+    hasPreviousPage: false,
+  });
   private readonly _loading = signal<boolean>(false);
   private readonly _error = signal<string | null>(null);
 
   readonly users = computed(() => this._users());
+  readonly meta = computed(() => this._meta());
   readonly loading = computed(() => this._loading());
   readonly error = computed(() => this._error());
 
@@ -32,13 +50,30 @@ export class UserService {
   }: {
     page?: number;
     pageSize?: number;
-  }): Observable<User[]> {
+  }): Observable<{
+    data: User[];
+    meta: PaginationMeta;
+  }> {
     this._loading.set(true);
     this._error.set(null);
 
     return this.http.get<User[]>(this.baseUrl).pipe(
-      map((res) => res.slice((page - 1) * pageSize, page * pageSize)),
-      tap((users) => this._users.set(users)),
+      delay(500),
+      map((res) => ({
+        data: res.slice((page - 1) * pageSize, page * pageSize),
+        meta: {
+          total: res.length,
+          page,
+          pageSize,
+          totalPages: Math.ceil(res.length / pageSize),
+          hasNextPage: page < Math.ceil(res.length / pageSize),
+          hasPreviousPage: page > 1,
+        },
+      })),
+      tap(({ data, meta }) => {
+        this._users.set(data);
+        this._meta.set(meta);
+      }),
       catchError((err) => {
         this._error.set(err.message);
         return throwError(() => new Error(err.message));
