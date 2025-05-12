c5c120e4f74b893d1df47fc62fb176e45d0c54fe
shj <josh2kv@gmail.com>
2025-05-12 19:03:40 +0900

    [hw30] Implement sequential progress bar

--- hw/hw30-progress-bars/src/app/components/advanced-progress-bar-generator/advanced-progress-bar-generator.component.ts
+++ hw/hw30-progress-bars/src/app/components/advanced-progress-bar-generator/advanced-progress-bar-generator.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { AdvancedProgressBarComponent } from './advanced-progress-bar/advanced-progress-bar.component';
+
+interface ProgressBarState {
+  id: number;
+  isActive: boolean;
+  isCompleted: boolean;
+}
 
 @Component({
   selector: 'app-advanced-progress-bar-generator',
@@ -8,4 +14,51 @@ import { AdvancedProgressBarComponent } from './advanced-progress-bar/advanced-p
   templateUrl: './advanced-progress-bar-generator.component.html',
   styleUrl: './advanced-progress-bar-generator.component.css',
 })
-export class AdvancedProgressBarGeneratorComponent {}
+export class AdvancedProgressBarGeneratorComponent {
+  _progressBars = signal<ProgressBarState[]>([]);
+  progressBars = computed(() => this._progressBars());
+
+  currentId = -1;
+
+  addProgressBar() {
+    const lastBar = this.progressBars()[this.progressBars().length - 1];
+
+    // first bar
+    if (!lastBar) {
+      this.currentId++;
+    }
+
+    this._progressBars.update((bars) => [
+      ...bars,
+      {
+        id: bars.length,
+        isActive: bars.length === 0 ? true : lastBar.isCompleted,
+        isCompleted: false,
+      },
+    ]);
+  }
+
+  startNext() {
+    this.currentId++;
+    console.log('startNext', this.currentId);
+    if (this.currentId < this.progressBars().length) {
+      this._progressBars.update((bars) => {
+        bars[this.currentId].isActive = true;
+        return bars;
+      });
+    }
+  }
+
+  onCompleted(id: number) {
+    if (id < this.progressBars().length) {
+      this._progressBars.update((bars) => {
+        console.log('onCompleted', bars);
+        bars[id].isActive = false;
+        bars[id].isCompleted = true;
+        return bars;
+      });
+    }
+
+    this.startNext();
+  }
+}
--- hw/hw30-progress-bars/src/app/components/advanced-progress-bar-generator/advanced-progress-bar/advanced-progress-bar.component.ts
+++ hw/hw30-progress-bars/src/app/components/advanced-progress-bar-generator/advanced-progress-bar/advanced-progress-bar.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+  OnChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-advanced-progress-bar',
@@ -7,14 +14,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './advanced-progress-bar.component.html',
   styleUrl: './advanced-progress-bar.component.css',
 })
-export class AdvancedProgressBarComponent {
+export class AdvancedProgressBarComponent implements OnChanges {
   @Input() id = 0;
-  @Input() progress = 0;
   @Input() isActive = false;
   @Output() completed = new EventEmitter<number>();
 
   duration = 2000;
   interval = 500;
+  progress = 0;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes', changes);
+    // Check if isActive property has changed
+    if (changes['isActive'] && changes['isActive'].currentValue === true) {
+      this.startProgress();
+    }
+  }
 
   startProgress() {
     if (!this.isActive) return;
@@ -31,7 +46,11 @@ export class AdvancedProgressBarComponent {
       if (this.progress >= 100) {
         this.progress = 100;
         clearInterval(intervalId);
-        this.completed.emit(this.id);
+
+        // wait for the interval to finish
+        setTimeout(() => {
+          this.completed.emit(this.id);
+        }, this.interval);
       }
     }, this.interval - 100);
   }
